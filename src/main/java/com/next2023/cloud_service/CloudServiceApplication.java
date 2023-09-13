2c6d382eb6f8b3f68e00fab54d41e3b84ac7d9e7
marcosgrac <mgs8cin@gmail.com>
2023-09-12 22:52:35 -0300

    Versão inicial do código NEXT_CLOUD_SERVICE

---
+++ src/main/java/com/next2023/cloud_service/CloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.next2023.cloud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next2023/cloud_service/CloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next2023.cloud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
