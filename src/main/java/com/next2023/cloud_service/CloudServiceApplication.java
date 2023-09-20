01f7a90e937a417aa0b876d6475360a16f6cf817
marcosgrac <mgs8cin@gmail.com>
2023-09-20 12:11:53 -0300

    re-inclusão do application.properties e adicionado @ComponentScan na classe: CloudServiceApplication

--- src/main/java/com/next2023/cloud_service/CloudServiceApplication.java
+++ src/main/java/com/next2023/cloud_service/CloudServiceApplication.java
@@ -3,8 +3,10 @@ package com.next2023.cloud_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan({"controllers", "com.next2023.cloud_service.*"})
 public class CloudServiceApplication {
 
 	public static void main(String[] args) {
