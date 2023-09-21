750a7b763099f9f6ea1432dd5a1ab254acae07c4
romulo.filho <romulo.filho@dataprev.gov.br>
2023-09-21 19:51:19 -0300

    Ajustes para rodar ponto a ponto

--- src/main/java/com/next2023/cloud_service/CloudServiceApplication.java
+++ src/main/java/com/next2023/cloud_service/CloudServiceApplication.java
@@ -2,10 +2,9 @@ package com.next2023.cloud_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @ComponentScan({"controllers","com.next2023.cloud_service.repositories.*", "com.next2023.cloud_service.*"})
 public class CloudServiceApplication {
 
